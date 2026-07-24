3500a7c8c4e9c90a3cba3e5c9011b887b1548db3
seeyoufxp <seeyoufxp@gmail.com>
2026-07-24 16:30:10 +0800

    添加AI和天气配置

--- seeyou-community-be/seeyou-ai/src/main/java/com/seeyou/ai/AiApplication.java
+++ seeyou-community-be/seeyou-ai/src/main/java/com/seeyou/ai/AiApplication.java
@@ -1,13 +1,12 @@
 package com.seeyou.ai;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.ai.autoconfigure.openai.OpenAiAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication(scanBasePackages = "com.seeyou", exclude = {DataSourceAutoConfiguration.class, OpenAiAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages = "com.seeyou", exclude = {DataSourceAutoConfiguration.class})
 public class AiApplication {
 
     public static void main(String[] args) {
